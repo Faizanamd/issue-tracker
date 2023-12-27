63d14050de8846f6c802d244dd27dcd726061535
FZNAhmad <faiz818791@gmail.com>
2023-12-27 18:32:12 +0530

    atlast got connected

--- src/config/connectToMongo.js
+++ src/config/connectToMongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoose = async() =>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/issuetracker");
+        await mongoose.connect("mongodb+srv://famdizan8391:2OyYjngmY2NBBgZX@cluster0.kn9rmf3.mongodb.net/?retryWrites=true&w=majority");
         console.log("Mongodb connected using mongoose");
     }catch(err){
         console.log("Error while connecting to db ", err);
