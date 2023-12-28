7f43a7881af36ecdd97b2e22e5729254c3cad18c
FZNAhmad <faiz818791@gmail.com>
2023-12-28 22:24:37 +0530

    comments added

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
+// Importing necessary modules and libraries
 import express from 'express';
 import path from 'path';
 import ejsLayout from 'express-ejs-layouts';
+
+// Importing controllers for handling project and issue-related actions
 import ProjectController from './src/controller/project.controller.js';
 import IssueController from './src/controller/issueController.js';
+
+// Creating instances of controllers
 const projectController = new ProjectController();
 const issueController = new IssueController();
-const app =express();
+
+// Creating an Express application
+const app = express();
+
+// Serving static files from the 'public' directory
 app.use(express.static('public'));
+
+// Using EJS layout middleware for rendering views
 app.use(ejsLayout);
+
+// Parsing JSON and URL-encoded data in requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Set the view engine as EJS
+
+// Setting the view engine as EJS
 app.set('view engine', 'ejs');
 
-// Set the views directory
+// Setting the views directory
 app.set('views', path.join(path.resolve(), 'src', 'views'));
 
-app.get('/projects/delete/:id', projectController.deleteProjectController)
+// Routes for handling project-related actions
+app.get('/projects/delete/:id', projectController.deleteProjectController);
 app.get('/projects/:projectId/delete/:id', projectController.deleteIssueController);
-
-
 app.post('/projects/:id/filter', issueController.filterBasedOnErrorTypes);
 app.post('/projects/:id/search', issueController.searchIssueController);
 app.get('/projects/:id', projectController.showProjectController);
+app.post('/newProject', projectController.addNewProjectContrller);
 
-app.post('/newProject', projectController.addNewProjectContrller)
+// Routes for handling issue-related actions
 app.post('/newIssue', issueController.addNewIssueController);
 
-app.get('/', projectController.issueHomePage)
+// Default route for the home page
+app.get('/', projectController.issueHomePage);
 
-
+// Exporting the Express application instance
 export default app;
--- src/controller/project.controller.js
+++ src/controller/project.controller.js
@@ -1,54 +1,78 @@
+// Importing repositories
 import IssueRepository from "../models/issue.repository.js";
 import ProjectRepository from "../models/project.repository.js";
 
+// Creating instances of repositories
 const projectRepository = new ProjectRepository();
 const issueRepository = new IssueRepository();
 
-export default class ProjectController{
-    async issueHomePage(req, res){
+// Exporting the ProjectController class
+export default class ProjectController {
+
+    // Controller method for rendering the home page with all projects
+    async issueHomePage(req, res) {
+        // Retrieving all projects from the project repository
         const projects = await projectRepository.getAllProjects();
-        // console.log(projects);
-        // console.log("Home page got printeed")
-        res.render('home', {allProjects: projects});
+        
+        // Rendering the home page with project data
+        res.render('home', { allProjects: projects });
     }
-    async addNewProjectContrller(req, res){
+
+    // Controller method for adding a new project
+    async addNewProjectContrller(req, res) {
         console.log(req.body);
-        try{
+        try {
+            // Adding a new project using the project repository
             const newIssue = await projectRepository.addNewIssueRepository(req.body);
-            // console.log("add New project got printed");
-            if(newIssue){
+            
+            // Redirecting to the home page after adding the project
+            if (newIssue) {
                 res.redirect('/');
-            }else{
+            } else {
                 res.render('home');
             }
-        }
-        catch(err){
+        } catch (err) {
+            // Handling and logging any errors that occur
             console.log(err)
             res.redirect('/')
         }
     }
-    async showProjectController(req , res){
+
+    // Controller method for rendering a project page with its issues
+    async showProjectController(req, res) {
         const id = req.params.id;
-        const project  = await projectRepository.getProjectById(id);
-        // console.log("Project", project);
+
+        // Retrieving project information and its associated issues from repositories
+        const project = await projectRepository.getProjectById(id);
         const issues = await issueRepository.getIssuesById(id);
         
-        // console.log("issues", issues);
-        res.render("projectPage", {project: project,issues: issues, projectId: id})
+        // Rendering the project page with project and issue data
+        res.render("projectPage", { project: project, issues: issues, projectId: id });
     }
-    async deleteProjectController(req,res){
+
+    // Controller method for deleting a project and its associated issues
+    async deleteProjectController(req, res) {
         console.log(req.body);
         console.log(req.params.id);
+
+        // Deleting project and its issues using project and issue repositories
         await projectRepository.deletProjectRepo(req.params.id);
         await issueRepository.deleteIssueByProjectId(req.params.id);
+
+        // Redirecting to the home page after deletion
         res.redirect('/');
     }
-    async deleteIssueController(req, res){
+
+    // Controller method for deleting a specific issue within a project
+    async deleteIssueController(req, res) {
         console.log(req.body);
         console.log(req.params.projectId);
         console.log(req.params.id);
-        await issueRepository.detelteIssyeById(req.params.id);
-        res.redirect(`/projects/${req.params.projectId}`)
 
+        // Deleting a specific issue by its ID using the issue repository
+        await issueRepository.detelteIssyeById(req.params.id);
+
+        // Redirecting to the project page after deleting the issue
+        res.redirect(`/projects/${req.params.projectId}`);
     }
 }
